84a29ae072edcd0aa38c48a603402dbaae3e01c2
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-29 08:07:47 -0400

    Update postgresqld.sh

--- postgresqld.sh
+++ postgresqld.sh
@@ -1,7 +1,6 @@
-#still need to find the way
-#for referemce only
-[program:postgresql]
-command=/bin/su postgres -c '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf'
-stdout_logfile=/var/log/supervisor/%(program_name)s.log
-stderr_logfile=/var/log/supervisor/%(program_name)s.log
-stopsignal=6
+#!/bin/sh
+### In postgresd.sh (make sure this file is chmod +x):
+# `/sbin/setuser postgres` runs the given command as the user `postgres`.
+# If you omit that part, the command will be run as root.
+
+exec /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1
