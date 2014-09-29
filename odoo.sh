16ac891542b3a28cda247cb97c943fd0040a1447
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-29 09:54:40 -0400

    Update odoo.sh

--- odoo.sh
+++ odoo.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-### In mysqld.sh (make sure this file is chmod +x):
-# `/sbin/setuser mysql` runs the given command as the user `mysql`.
+### In odoo.sh (make sure this file is chmod +x):
+# `/sbin/setuser openerp` runs the given command as the user `openerp`.
 # If you omit that part, the command will be run as root.
 
-exec /sbin/setuser mysql /usr/bin/mysqld_safe >>/var/log/mysqld.log 2>&1
-
-
-#for reference only
-[program:odoo]
-command=start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid openerp --make-pidfile --exec /odoo/openerp-server -- --config=/etc/openerp/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log
-stdout_logfile=/var/log/supervisor/%(program_name)s.log
-stderr_logfile=/var/log/supervisor/%(program_name)s.log
-stopsignal=6
+exec /sbin/setuser openerp start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid openerp --make-pidfile --exec /odoo/openerp-server -- --config=/etc/openerp/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log >>/var/log/mysqld.log 2>&1
