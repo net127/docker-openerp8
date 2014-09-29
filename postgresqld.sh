c5e9b23a480adebcf63a585d5cb10771e39535bb
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-29 08:00:57 -0400

    Update postgresqld.sh

--- postgresqld.sh
+++ postgresqld.sh
@@ -1,1 +1,7 @@
 #still need to find the way
+#for referemce only
+[program:postgresql]
+command=/bin/su postgres -c '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf'
+stdout_logfile=/var/log/supervisor/%(program_name)s.log
+stderr_logfile=/var/log/supervisor/%(program_name)s.log
+stopsignal=6
