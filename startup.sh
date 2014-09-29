597d61a989e0035901923dc9fb201ae7527f1b2f
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-29 07:51:52 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,14 +6,14 @@ set -e
 if [ -f /etc/configured ]; then
         echo 'already configured'
 else
-        openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
-        sed -i 's/ServerName example.com/ServerName $CN/' /etc/apache2/conf.d/owncloud.conf
+        #openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
+        #sed -i 's/ServerName example.com/ServerName $CN/' /etc/apache2/conf.d/owncloud.conf
         #to include the domain to the hosts
-        echo 127.0.0.1 $CN >> /etc/hosts
+        #echo 127.0.0.1 $CN >> /etc/hosts
         # for security change permission of folder and files
-        chown -R www-data:www-data /var/www/owncloud
-        find /var/www/owncloud -type d -exec chmod 750 {} \;
-        find /var/www/owncloud -type f -exec chmod 640 {} \;
+        #chown -R www-data:www-data /var/www/owncloud
+        #find /var/www/owncloud -type d -exec chmod 750 {} \;
+        #find /var/www/owncloud -type f -exec chmod 640 {} \;
         
         
         POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
