4ddc161413912a4396d660a1d16bc12adf680f03
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-30 01:21:15 -0400

    Update odoo.sh

--- odoo.sh
+++ odoo.sh
@@ -3,4 +3,4 @@
 # `/sbin/setuser openerp` runs the given command as the user `openerp`.
 # If you omit that part, the command will be run as root.
 
-exec /sbin/setuser openerp start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid openerp --make-pidfile --exec /odoo/openerp-server -- --config=/etc/openerp/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log >>/var/log/mysqld.log 2>&1
+exec start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid openerp --make-pidfile --exec /sbin/setuser openerp /usr/bin/openerp-server --config=/etc/openerp/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log >>/var/log/mysqld.log 2>&1
